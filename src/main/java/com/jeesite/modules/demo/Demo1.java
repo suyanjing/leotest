195fd389338a8ab8d50bd93aeca5223942c15a0a
crazynn <suyanjingnn@163.com>
2019-12-02 21:31:19 +0800

    add Demo

--- src/main/java/com/jeesite/modules/demo/Demo1.java
+++ src/main/java/com/jeesite/modules/demo/Demo1.java
@@ -1,4 +1,8 @@
 package com.jeesite.modules.demo;
 
 public class Demo1 {
+
+    public static void main(String[] args) {
+        System.out.println("测试一");
+    }
 }
--- src/main/java/com/jeesite/modules/demo/Demo2.java
+++ src/main/java/com/jeesite/modules/demo/Demo2.java
@@ -1,4 +1,8 @@
 package com.jeesite.modules.demo;
 
 public class Demo2 {
+
+    public static void main(String[] args) {
+        System.out.println("测试3");
+    }
 }
